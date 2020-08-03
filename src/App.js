71afca928046b136ba56ca76ab3bece5c0d8efad
Zach Shirah <zachshirah@MacBook-Pro.local>
2020-08-03 16:47:34 -0400

    added react-router-dom. tested that it is working by creating and linking the play page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+
 import "./App.css";
-import PlayingSurface from "./PlayingSurface";
 import "./tailwind.css";
-import Title from './Title'
-import Button from './Button'
+
+import PlayingSurface from "./PlayingSurface";
+import Title from "./Title";
+import Button from "./Button";
 import HighScore from "./HighScore";
 
 function App() {
   return (
-    <div className="bg-gray-200 w-screen h-screen flex flex-col flex-no-wrap items-center justify-around mb-2">
-      <Title />
-      <Button buttonText={"New Game"} />
-      <Button buttonText={"Leaderboard"} />
-      <Button buttonText={"How to?"} />
-      <HighScore />
-
-      {/* <PlayingSurface /> */}
+    <div>
+      <Route path="/" exact>
+        <div className="bg-gray-200 w-screen h-screen flex flex-col flex-no-wrap items-center justify-around mb-2">
+          <Title />
+          <Link to='/play'>
+            <Button buttonText={"New Game"}/>
+          </Link>
+          <Button buttonText={"Leaderboard"} />
+          <Button buttonText={"How to?"} />
+          <HighScore />
+        </div>
+      </Route>
+      <Route path="/play">
+        <PlayingSurface />
+      </Route>
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,16 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Button({ buttonText }) {
   return (
     <div>
-      <button
-        className='w-48 bg-blue-600 rounded-lg text-gray-200 text-2xl py-4 font-bold shadow-lg hover:shadow-2xl hover:bg-blue-700'
-        
-      >
+      <button className="w-48 bg-blue-600 rounded-lg text-gray-200 text-2xl py-4 font-bold shadow-lg hover:shadow-2xl hover:bg-blue-700">
         {buttonText}
       </button>
     </div>
-  )
+  );
 }
 
-export default Button
+export default Button;
