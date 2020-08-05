94ed625e8d39fca9247038d60a1767a7288b352e
Zach Shirah <zachshirah@MacBook-Pro.local>
2020-08-05 12:28:41 -0400

    made it responsive

--- src/App.js
+++ src/App.js
@@ -14,12 +14,10 @@ import TitleSmall from "./TitleSmall";
 import HighScoreSmall from "./HighScoreSmall";
 import Axios from "axios";
 
-
-
 function App() {
-  const [leaderboardData, updateLeaderboardData] = useState([])
-  const [highScore, updateHighScore] = useState(0)
-  const [highScoreUsername, updateHighScoreUsername] = useState('')
+  const [leaderboardData, updateLeaderboardData] = useState([]);
+  const [highScore, updateHighScore] = useState(0);
+  const [highScoreUsername, updateHighScoreUsername] = useState("");
 
   useEffect(() => {
     const apiCall = async () => {
@@ -32,15 +30,13 @@ function App() {
         }
       );
       console.log(data.data.records[0].fields.score);
-      updateLeaderboardData(data.data.records)
-      updateHighScore(data.data.records[0].fields.score)
+      updateLeaderboardData(data.data.records);
+      updateHighScore(data.data.records[0].fields.score);
       updateHighScoreUsername(data.data.records[0].fields.username);
-      
     };
     apiCall();
   }, []);
 
-  
   return (
     <div className="w-screen h-screen bg-gray-100">
       <Route path="/" exact>
@@ -62,15 +58,26 @@ function App() {
         </div>
       </Route>
       <Route path="/play">
-        <div className="w-screen h-screen flex flex-col flex-no-wrap items-center justify-around mb-2">
+        <div className="w-screen  h-screen flex flex-col flex-no-wrap items-center sm:justify-around md:justify-evenly mb-2">
           <Title />
-          {/* <HighScoreSmall />
+          <div className="flex flex-row flex-no-wrap m-4 justify-around">
+            <div className="hidden md:flex md:flex-col md:flex-no-wrap md:w-1/2 md:border md:border-blue-800 md:rounded-lg md:bg-blue-300 md:bg-opacity-75 md:p-4 lg:w-1/3">
+              <h2 className="text-center text-4xl font-bold">Leaderboard</h2>
+              <Leaderboard data={leaderboardData} />
+            </div>
+            {/* <HighScoreSmall />
           <HighScoreSmall /> */}
-          <PlayingSurface />
-          <HighScore
-            highScore={highScore}
-            highScoreUsername={highScoreUsername}
-          />
+            <div className="flex flex-col flex-no-wrap items-center justify-around">
+              <PlayingSurface />
+              <HighScore
+                highScore={highScore}
+                highScoreUsername={highScoreUsername}
+              />
+            </div>
+            <div className="w-1/3 hidden lg:flex p-4">
+              <HowTo />
+            </div>
+          </div>
         </div>
       </Route>
       <Route path="/leaderboard">
--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -3,22 +3,30 @@ import React from "react";
 function Leaderboard({ data }) {
   return (
     <div>
-      <table className="table-auto">
+      <table className={`table-auto flex flex-col items-center`}>
         <thead>
           <tr>
             {/* <th className="py-2">rank</th> */}
-            <th className="px-4 py-2">name</th>
-            <th className="px-4 py-2">score</th>
-            <th className="px-4 py-2">date</th>
+            <th className="sm:px-4 sm:py-2 text-center lg:px-8 lg:py-4 lg:text-2xl">
+              Name
+            </th>
+            <th className="px-4 py-2 text-center lg:text-2xl">Score</th>
+            <th className="px-4 py-2 w-48 text-center lg:text-2xl">Date</th>
           </tr>
         </thead>
         <tbody>
           {data.map((row) => (
             <tr key={row.fields.ID}>
               {/* <td className="border px-4 py-2 font-bold"></td> */}
-              <td className="border px-4 py-2">{row.fields.username}</td>
-              <td className="border px-4 py-2">{row.fields.score}</td>
-              <td className="border px-4 py-2">{row.fields.date}</td>
+              <td className="border sm:px-4 sm:py-2 text-center lg:px-8 lg:py-4 lg:text-2xl">
+                {row.fields.username}
+              </td>
+              <td className="border sm:px-4 sm:py-2 text-center lg:text-2xl">
+                {row.fields.score}
+              </td>
+              <td className="border sm:px-4 sm:py-2 w-48 text-center lg:text-2xl">
+                {row.fields.date}
+              </td>
             </tr>
           ))}
         </tbody>
