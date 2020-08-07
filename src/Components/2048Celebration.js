10a13fd19687e4b46f23c5b5bf05648fc5568c54
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-07 10:40:49 -0400

    updated 2048 celebration

--- src/Components/2048Celebration.js
+++ src/Components/2048Celebration.js
@@ -6,18 +6,20 @@ function _2048Celebration({ hit2048Tile, updateHit2048Tile }) {
     <AnimatePresence>
       {hit2048Tile && (
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1, rotate: 360 }}
+          initial={{ scale: 0 }}
+          animate={{ rotate: 360, scale: 1 }}
           transition={{
-            duration: 1,
-            times: 4,
+            type: "spring",
+            stiffness: 260,
+            damping: 20,
           }}
-          exit={{ opacity: 0 }}
+          exit={{ opacity: 0, scale: 0 }}
           className={`bg-teal-700 border border-gray-800 h-64 w-64 rounded-md text-gray-900 font-semibold text-2xl text-center fixed left-auto py-16`}
           value="2048"
           onClick={() => updateHit2048Tile(false)}
         >
-          Congrats! You got to the 2048 tile. Click the tile to continue playing.
+          Congrats! You got to the 2048 tile. Click the tile to continue
+          playing.
         </motion.div>
       )}
     </AnimatePresence>
