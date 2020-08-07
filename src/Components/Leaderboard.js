92073ee051126215fa756130cd3509122110d3fa
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-07 10:10:42 -0400

    changed leaderboard font size with lg screens from 2xl to xl becasue it was causing the title to get messed up on desktop

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -14,24 +14,24 @@ function Leaderboard({ data }) {
         <thead>
           <tr>
             {/* <th className="py-2">rank</th> */}
-            <th className="px-2 py-1 text-center lg:px-8 lg:py-4 lg:text-2xl">
+            <th className="px-2 py-1 text-center lg:px-4 lg:py-2 lg:text-xl">
               Name
             </th>
-            <th className="px-2 py-1 text-center lg:text-2xl">Score</th>
-            <th className="px-2 py-1 w-48 text-center lg:text-2xl">Date</th>
+            <th className="px-2 py-1 text-center lg:text-xl">Score</th>
+            <th className="px-2 py-1 w-48 text-center lg:text-xl">Date</th>
           </tr>
         </thead>
         <tbody>
           {data.map((row) => (
             <tr key={row.fields.ID}>
               {/* <td className="border px-4 py-2 font-bold"></td> */}
-              <td className="border px-2 py-1 text-center lg:px-8 lg:py-4 lg:text-2xl">
+              <td className="border px-2 py-1 text-center lg:px-4 lg:py-2 lg:text-xl">
                 {row.fields.username}
               </td>
-              <td className="border px-2 py-1 text-center lg:text-2xl">
+              <td className="border px-2 py-1 text-center lg:text-xl">
                 {row.fields.score}
               </td>
-              <td className="border px-2 py-1 w-48 text-center lg:text-2xl">
+              <td className="border px-2 py-1 w-48 text-center lg:text-xl">
                 {row.fields.date}
               </td>
             </tr>
